05ac2c652c04fbf23b87e4b1fc9de360f24fb7f5
Philipp Weissensteiner <mail@philippweissensteiner.com>
2014-07-04 21:36:41 +0200

    .on with specs

--- test/emitter.js
+++ test/emitter.js
@@ -1,13 +1,45 @@
+function Telephone(name) {
+    this.name = name;
+    Emitter.call(this);
+}
+
+Telephone.prototype = new Emitter();
+
 describe('Emitter', function(){
 
   it('should start with an empty callbacks object', function(){
-    var emitter = new Emitter();
+    var emitter = new Telephone();
     expect(emitter.callbacks).toEqual({});
   });
 
   describe('.on', function(){
     it('should add listeners', function(){
+      var phone = new Telephone();
 
+      var ring = function(){};
+      var vibrate = function(){};
+
+      phone.on('call', ring);
+      expect(phone.callbacks).toEqual({ 'call' : [ring] });
+
+      phone.on('call', vibrate);
+      expect(phone.callbacks).toEqual({ 'call' : [ring, vibrate] });
+
+      phone.on('sms', vibrate);
+      expect(phone.callbacks).toEqual({
+         'call' : [ring, vibrate],
+         'sms'  : [vibrate]
+       });
+
+      var ignore = function(){};
+      expect(function() {
+        phone.on(3, ignore);
+      }).toThrow(new Error('event is not a string literal!'));
+
+      var keyString = new String('call'); // we do not want instances of String
+      expect(function() {
+        phone.on(keyString, vibrate);
+      }).toThrow(new Error('event is not a string literal!'));
     });
   });
 
